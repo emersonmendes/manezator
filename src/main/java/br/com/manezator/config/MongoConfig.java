0644fce71b326733ced14b7a8920e5481f5a49af
Laisson.Silveira <laisson.r.silveira@gmail.com>
2017-03-18 16:40:16 -0300

    Fix mongodb user

--- src/main/java/br/com/manezator/config/MongoConfig.java
+++ src/main/java/br/com/manezator/config/MongoConfig.java
@@ -1,29 +1,59 @@
 package br.com.manezator.config;
 
-import java.net.UnknownHostException;
-
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import com.mongodb.MongoClient;
+import static java.util.Collections.singletonList;
 
 @Configuration
 @EnableMongoRepositories("br.com.manezator.repository")
-public class MongoConfig /*extends AbstractMongoConfiguration */{
-	
-	@Bean
-    public MongoDbFactory mongoDbFactory() throws UnknownHostException{
-        return new SimpleMongoDbFactory(new MongoClient("localhost", 27017), "manezatordb");
-    }
-  
+public class MongoConfig extends AbstractMongoConfiguration {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
     @Bean
-    public MongoOperations mongoOperations() throws UnknownHostException{
-        return new MongoTemplate(mongoDbFactory());
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+	return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+	return new LocalValidatorFactoryBean();
+    }
+
+
+    @Override
+    public String getDatabaseName() {
+	return database;
+    }
+
+    @Override
+    @Bean
+    public Mongo mongo() throws Exception {
+	return new MongoClient(singletonList(new ServerAddress(host, port)),
+			singletonList(MongoCredential.createCredential(username,database, password.toCharArray())));
     }
 
 }
