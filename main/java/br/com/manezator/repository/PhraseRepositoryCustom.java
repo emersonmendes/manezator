ba1b5b44fe915c8ab59bd4a56f063f4f10c4c3fe
Laisson.Silveira <laisson.r.silveira@gmail.com>
2017-03-20 19:34:26 -0300

    remove folder

--- main/java/br/com/manezator/repository/PhraseRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package br.com.manezator.repository;
-
-import br.com.manezator.model.Phrase;
-
-public interface PhraseRepositoryCustom  {
-
-	Phrase findByTextAndManezes(String lowerCase, Boolean manezes);
-	
-}
