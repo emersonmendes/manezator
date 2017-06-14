460e679fc1bce4e376fac6ad785a29926ed7683b
Emerson Mendes <emerson.emendes@gmail.com>
2017-06-13 23:30:06 -0300

    trocando pra npm as dependencias web

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# Wifeed
+# Manezator
 
-- [Install mongodb](https://docs.mongodb.com/manual/installation/)
+
+-- [Install mongodb](https://docs.mongodb.com/manual/installation/)
+
+-- create dev database:
+
+- db.createUser({user:"manezator", pwd:"manezator", roles:[{role:"dbOwner", db: "manezatordb"}]})
+
+-- run app
 - mvn clean install 
 - mvn spring-boot:run
-
 
--- src/main/java/br/com/manezator/config/MongoConfig.java
+++ src/main/java/br/com/manezator/config/MongoConfig.java
@@ -18,42 +18,41 @@ import static java.util.Collections.singletonList;
 @EnableMongoRepositories("br.com.manezator.repository")
 public class MongoConfig extends AbstractMongoConfiguration {
 
-    @Value("${spring.data.mongodb.host}")
-    private String host;
+	@Value("${spring.data.mongodb.host}")
+	private String host;
 
-    @Value("${spring.data.mongodb.port}")
-    private Integer port;
+	@Value("${spring.data.mongodb.port}")
+	private Integer port;
 
-    @Value("${spring.data.mongodb.username}")
-    private String username;
+	@Value("${spring.data.mongodb.username}")
+	private String username;
 
-    @Value("${spring.data.mongodb.database}")
-    private String database;
+	@Value("${spring.data.mongodb.database}")
+	private String database;
 
-    @Value("${spring.data.mongodb.password}")
-    private String password;
+	@Value("${spring.data.mongodb.password}")
+	private String password;
 
-    @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener() {
-	return new ValidatingMongoEventListener(validator());
-    }
+	@Bean
+	public ValidatingMongoEventListener validatingMongoEventListener() {
+		return new ValidatingMongoEventListener(validator());
+	}
 
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-	return new LocalValidatorFactoryBean();
-    }
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		return new LocalValidatorFactoryBean();
+	}
 
+	@Override
+	public String getDatabaseName() {
+		return database;
+	}
 
-    @Override
-    public String getDatabaseName() {
-	return database;
-    }
-
-    @Override
-    @Bean
-    public Mongo mongo() throws Exception {
-	return new MongoClient(singletonList(new ServerAddress(host, port)),
-			singletonList(MongoCredential.createCredential(username,database, password.toCharArray())));
-    }
+	@Override
+	@Bean
+	public Mongo mongo() throws Exception {
+		return new MongoClient(singletonList(new ServerAddress(host, port)),
+				singletonList(MongoCredential.createCredential(username, database, password.toCharArray())));
+	}
 
 }
