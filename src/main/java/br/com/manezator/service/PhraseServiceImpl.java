734b04fc162fa2ee5c1694b4c7d10f96bb043f10
Emerson Mendes <emerson.emendes@gmail.com.br>
2020-03-04 20:25:52 -0300

    melhoria na tradução

--- src/main/java/br/com/manezator/service/PhraseServiceImpl.java
+++ src/main/java/br/com/manezator/service/PhraseServiceImpl.java
@@ -1,8 +1,11 @@
 package br.com.manezator.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,35 +17,70 @@ import br.com.manezator.repository.PhraseRepository;
 @Service
 public class PhraseServiceImpl implements PhraseService {
 
-	@Autowired
+    public static final char SPACE = ' ';
+    @Autowired
 	public PhraseRepository phraseRepository;
 	
-	public Phrase translate(String originalPhrase, Boolean manezes) {
-		return phraseRepository.findOneByTextAndManezes(originalPhrase.toLowerCase().trim(), manezes);
-	}
+	public Phrase translate(String originalPhraseText, Boolean manezes) {
 
-	@Override
-	public void sendTraduction(
-		String originalPhrase, 
-		String translatedPhrase, 
-		Boolean manezes
-	) {
+ 	    final Phrase phrase = phraseRepository.findOneByTextAndManezes(originalPhraseText.toLowerCase().trim(), manezes);
+
+        final String[] splitPhrase = originalPhraseText.split(" ");
+
+        if(phrase != null || splitPhrase.length == 1){
+            return phrase;
+        }
+
+        AtomicReference<String> translatedPhraseTextAR = new AtomicReference<>("");
+
+        Stream.of(splitPhrase).forEach(text -> {
+
+            final String trimmedText = text.toLowerCase().trim();
+
+            if(trimmedText.equals("")){
+               return;
+            }
+
+            final Phrase p = phraseRepository.findOneByTextAndManezes(trimmedText, manezes);
+
+            if(p == null){
+                translatedPhraseTextAR.set(translatedPhraseTextAR.get().concat(trimmedText));
+            } else {
+                translatedPhraseTextAR.set(translatedPhraseTextAR.get().concat(p.getTraductions().get(0).getText()));
+            }
+
+            translatedPhraseTextAR.set(translatedPhraseTextAR.get() + SPACE);
+
+        });
+
+        return buildPhrase(originalPhraseText, translatedPhraseTextAR.get(), manezes);
+
+    }
+
+    private Phrase buildPhrase(String originalPhraseText, String translatedPhraseText, Boolean manezes) {
+        final Phrase phrase = new Phrase();
+        phrase.setManezes(manezes);
+        phrase.setRegister(new Date());
+        phrase.setText(originalPhraseText);
+        final Traduction traduction = new Traduction();
+        traduction.setText(translatedPhraseText);
+        phrase.setTraductions(Collections.singletonList(traduction));
+        return phrase;
+    }
+
+    @Override
+	public void sendTraduction(String originalPhrase, String translatedPhrase, Boolean manezes) {
 		originalPhrase = originalPhrase.toLowerCase().trim();
 		translatedPhrase = translatedPhrase.toLowerCase().trim();
 		saveTraduction(originalPhrase, translatedPhrase, manezes);
 		saveTraduction(translatedPhrase, originalPhrase, !manezes);
 	}
 
-	private void saveTraduction(
-		String originalPhrase, 
-		String translatedPhrase, 
-		Boolean manezes
-	) {
+	private void saveTraduction(String originalPhrase, String translatedPhrase, Boolean manezes) {
 		
 		Phrase phrase = phraseRepository.findOneByTextAndManezes(originalPhrase, manezes);
 		
 		if(phrase == null){
-			
 			Phrase newPhrase = new Phrase();
 			newPhrase.setManezes(manezes);
 			newPhrase.setText(originalPhrase);
@@ -50,7 +88,6 @@ public class PhraseServiceImpl implements PhraseService {
 			traductions.add(new Traduction(1,translatedPhrase));
 			newPhrase.setTraductions(traductions);
 			phrase = newPhrase; 
-			
 		} else {
 			
 			List<Traduction> traductions = phrase.getTraductions();
